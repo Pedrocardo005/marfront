5a20146923c6fba8313d9b69e3eee9cf186f0ea1
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-01-29 11:47:27 -0300

    Add componente do botao de back top #7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,10 +8,22 @@ import { HeaderComponent } from './core/layout/header/header.component';
 import { HomeComponent } from './features/home/routes/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './core/layout/footer/footer.component';
+import { BackTopComponent } from './core/layout/back-top/back-top.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    BackTopComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
