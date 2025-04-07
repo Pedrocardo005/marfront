039e8535ee34642ea4f05143ee558b6552b68143
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-04-06 21:54:23 -0300

    :wastebasket: remove: Remove template antigo
    By: Pedro
    Refs: #23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackTopComponent } from './core/layout/back-top/back-top.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
 import { HeaderComponent } from './core/layout/header/header.component';
 import { HomeComponent } from './features/home/routes/home/home.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { FooterComponent } from './core/layout/footer/footer.component';
-import { BackTopComponent } from './core/layout/back-top/back-top.component';
 
-@NgModule({ declarations: [
-        AppComponent,
-        HeaderComponent,
-        HomeComponent,
-        FooterComponent,
-        BackTopComponent,
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    BackTopComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+})
 export class AppModule {}
