48ce6d3ac43fd3e9b5d236cfabbc65abd7751071
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-04-06 22:00:43 -0300

    :wastebasket: remove: Remove back top component
    By: Pedro
    Refs: #23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,12 @@ import {
 } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BackTopComponent } from './core/layout/back-top/back-top.component';
 import { FooterComponent } from './core/layout/footer/footer.component';
 import { HeaderComponent } from './core/layout/header/header.component';
 import { HomeComponent } from './features/home/routes/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    FooterComponent,
-    BackTopComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, FooterComponent],
   bootstrap: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [provideHttpClient(withInterceptorsFromDi())],
