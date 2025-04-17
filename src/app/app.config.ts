dfcc96ca972a35b068e3716266ad5c7fe5dcbc0e
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-04-17 16:31:46 -0300

    :globe_with_meridians: feat: Add internacionalizacao
    by: Pedro
    Refs: #46

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, Routes } from '@angular/router';
+import { provideTransloco } from '@jsverse/transloco';
 import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { LoginComponent } from './features/auth/pages/login/login.component';
 import { RegisterComponent } from './features/auth/pages/register/register.component';
 import { HomeComponent } from './features/home/pages/home/home.component';
+import { TranslocoHttpLoader } from './transloco-loader';
 
 const routes: Routes = [
   {
@@ -32,6 +34,16 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
       },
+    }),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['pt-BR', 'en', 'es', 'de'],
+        defaultLang: 'pt-BR',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
     }),
   ],
 };
