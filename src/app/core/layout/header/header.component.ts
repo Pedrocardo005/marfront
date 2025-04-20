3d815421252c0e5b7626608d6fc1cfe9204d71f3
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-04-20 17:29:09 -0300

    :sparkles: feat: Cria header
    By: Pedro
    Refs: #40

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslocoModule } from '@jsverse/transloco';
+import { MenuItem } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
+import { Menu } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  imports: [],
+  imports: [
+    MenubarModule,
+    InputTextModule,
+    TranslocoModule,
+    Menu,
+    RippleModule,
+    RouterLink,
+  ],
 })
-export class HeaderComponent {
-  isAuth = false;
+export class HeaderComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        items: [
+          {
+            label: 'CORE.LAYOUT.HEADER.DISCONNECT.LOGIN',
+          },
+          {
+            label: 'CORE.LAYOUT.HEADER.DISCONNECT.REGISTER',
+          },
+        ],
+      },
+    ];
+  }
 }
