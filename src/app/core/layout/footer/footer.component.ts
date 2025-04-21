aa6e9f1913e4fdca5854edb445b3f0596ea406a4
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-04-20 21:20:01 -0300

    :sparkles: feat: Ajusta footer
    By: Pedro
    Refs: #40

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  imports: [TranslocoModule],
 })
 export class FooterComponent {}
