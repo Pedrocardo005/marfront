065cbf57874303018e6d96b4f7f2c14b8c9f052c
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-06-26 21:00:21 -0300

    :sparkles: feat: Add texto e internacionalizacao
    By: Pedro
    Refs: #26

--- src/app/features/support/pages/support/support.component.ts
+++ src/app/features/support/pages/support/support.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslocoModule } from '@jsverse/transloco';
 import { SearchBarComponent } from '@shared/components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-support',
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent, TranslocoModule],
   templateUrl: './support.component.html',
   styleUrl: './support.component.css',
 })
