d92a28287b67f204f4466217bff9a37921732a01
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-06-29 12:58:58 -0300

    :sparkles: feat: Cria formulario
    By: Pedro
    Refs: #26

--- src/app/features/support/pages/support/support.component.ts
+++ src/app/features/support/pages/support/support.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslocoModule } from '@jsverse/transloco';
 import { SearchBarComponent } from '@shared/components/search-bar/search-bar.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { TextareaModule } from 'primeng/textarea';
+
+interface Option {
+  text: string;
+  value: number;
+}
 
 @Component({
   selector: 'app-support',
-  imports: [SearchBarComponent, TranslocoModule],
+  imports: [
+    SearchBarComponent,
+    TranslocoModule,
+    SelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    TextareaModule,
+    ButtonModule,
+  ],
   templateUrl: './support.component.html',
   styleUrl: './support.component.css',
 })
-export class SupportComponent {}
+export class SupportComponent implements OnInit {
+  options: Option[] | undefined;
+
+  optionSelected: Option | undefined;
+
+  email: string | undefined;
+
+  text: string | undefined;
+
+  ngOnInit() {
+    this.options = [
+      {
+        text: 'FEATURES.SUPPORT.MAIN-BAR.SELECT.OPTIONS.ONE-TEXT',
+        value: 1,
+      },
+      {
+        text: 'FEATURES.SUPPORT.MAIN-BAR.SELECT.OPTIONS.TWO-TEXT',
+        value: 2,
+      },
+      {
+        text: 'FEATURES.SUPPORT.MAIN-BAR.SELECT.OPTIONS.THREE-TEXT',
+        value: 3,
+      },
+      {
+        text: 'FEATURES.SUPPORT.MAIN-BAR.SELECT.OPTIONS.FOUR-TEXT',
+        value: 4,
+      },
+    ];
+
+    this.optionSelected = this.options[0];
+  }
+}
