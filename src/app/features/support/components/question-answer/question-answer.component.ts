381dbf8efd2fe25fe1730d38b7340a50297763d4
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-07-01 21:16:11 -0300

    :sparkles: feat: Cria sessao perguntas respostas
    By: Pedro
    Refs: #26

---
+++ src/app/features/support/components/question-answer/question-answer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-answer',
+  imports: [],
+  templateUrl: './question-answer.component.html',
+  styleUrl: './question-answer.component.css'
+})
+export class QuestionAnswerComponent {
+
+}
