4b549297d6d13f1b94019c52429ed66f36239571
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-09-12 20:36:40 -0300

    feat(login): Utiliza username como forma de login
    By: Pedro
    Refs: #29

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -3,7 +3,6 @@ import { FormsModule } from "@angular/forms";
 import { RouterLink } from "@angular/router";
 import { TranslocoPipe } from "@jsverse/transloco";
 import { SearchBarComponent } from "@shared/components/search-bar/search-bar.component";
-import { RegexTextsService } from "@shared/services/regex-texts.service";
 import { ButtonModule } from "primeng/button";
 import { InputTextModule } from "primeng/inputtext";
 import { PasswordModule } from "primeng/password";
@@ -23,17 +22,12 @@ import { PasswordModule } from "primeng/password";
   ],
 })
 export class LoginComponent {
-  formEmail = "";
+  formUsername = "";
   formPassword = "";
 
   loading = false;
 
-  constructor(private regexTextsService: RegexTextsService) {}
-
   isValidForm(): boolean {
-    return (
-      this.regexTextsService.isEmail(this.formEmail) &&
-      this.formPassword.length >= 8
-    );
+    return this.formUsername.length > 0 && this.formPassword.length >= 8;
   }
 }
