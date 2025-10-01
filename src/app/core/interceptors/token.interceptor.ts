11a2c72da26a48c310372386c609ccf331dba2ce
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-10-01 20:25:36 -0300

    feat(interceptor) Add interceptor checagem autenticado
    By: Pedro

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,14 +1,22 @@
 import { HttpInterceptorFn } from "@angular/common/http";
 import { inject } from "@angular/core";
+import { AuthService } from "@core/services/auth.service";
 import { TokenService } from "@core/services/token.service";
+import { catchError, throwError } from "rxjs";
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const token = inject(TokenService).getToken();
+  const authService = inject(AuthService);
 
   const request = req.clone({
     setHeaders: {
       ...(token ? { Authorization: `Bearer ${token}` } : {}),
     },
   });
-  return next(request);
+  return next(request).pipe(catchError((error) => {
+    if (error.status === 401) {
+      authService.logout();
+    }
+    return throwError(() => error);
+  }));
 };
