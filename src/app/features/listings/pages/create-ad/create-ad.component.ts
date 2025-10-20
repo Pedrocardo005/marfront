d66212dc1a669376ebf28221f60c4842138f9b11
Pedro da Conceicao <pedrocardo007@hotmail.com>
2025-10-20 20:26:09 -0300

    feat(create-ad) Add exibion options
    Refs: #17

--- src/app/features/listings/pages/create-ad/create-ad.component.ts
+++ src/app/features/listings/pages/create-ad/create-ad.component.ts
@@ -3,15 +3,19 @@ import { FormsModule } from '@angular/forms';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { SearchBarComponent } from "@shared/components/search-bar/search-bar.component";
 import { CatSubcatService } from '@shared/services/catsubcat.service';
+import { RadioButton } from 'primeng/radiobutton';
 import { TreeSelect } from 'primeng/treeselect';
 
 @Component({
   selector: 'app-create-ad',
-  imports: [SearchBarComponent, TranslocoPipe, TreeSelect, FormsModule],
+  imports: [SearchBarComponent, TranslocoPipe, TreeSelect, FormsModule, RadioButton],
   templateUrl: './create-ad.component.html',
   styleUrl: './create-ad.component.css'
 })
 export class CreateAdComponent implements OnInit {
+
+  exibitionType: string = 'sell';
+
   items: {
     key: string;
     label: string;
