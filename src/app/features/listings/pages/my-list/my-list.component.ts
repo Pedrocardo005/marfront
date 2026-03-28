f5843ec1714cdc01c65bd75f5fe7155a2e12ff95
Pedro da Conceicao <pedrocardo007@hotmail.com>
2026-03-28 18:25:40 -0300

    :sparkles: feat: Add my-list pagination

--- src/app/features/listings/pages/my-list/my-list.component.ts
+++ src/app/features/listings/pages/my-list/my-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { AnuncioUsuario } from "@features/listings/models/anuncio-usuario.model";
 import { AnunciosUserService } from "@features/listings/services/anuncios-user.service";
 import { TranslocoPipe } from "@jsverse/transloco";
@@ -7,22 +8,54 @@ import { AnuncioUserComponent } from "../../components/anuncio-user/anuncio-user
 
 @Component({
   selector: "app-my-list",
-  imports: [SearchBarComponent, TranslocoPipe, AnuncioUserComponent],
+  imports: [SearchBarComponent, TranslocoPipe, AnuncioUserComponent, CommonModule],
   templateUrl: "./my-list.component.html",
   styleUrl: "./my-list.component.css",
 })
 export class MyListComponent implements OnInit {
   anuncios: AnuncioUsuario[] = [];
 
+  currentPage: number = 1;
+  pageSize: number = 10;
+  totalCount: number = 0;
+  pageSizeOptions: number[] = [5, 10, 20, 50];
+
+  get totalPages(): number {
+    return Math.ceil(this.totalCount / this.pageSize);
+  }
+
+  get pages(): number[] {
+    const total = this.totalPages;
+    const current = this.currentPage;
+    const delta = 2;
+    const range: number[] = [];
+    const start = Math.max(1, current - delta);
+    const end = Math.min(total, current + delta);
+    for (let i = start; i <= end; i++) range.push(i);
+    return range;
+  }
+
   constructor(private readonly anunciosUserService: AnunciosUserService) { }
 
   ngOnInit(): void {
-    this.anunciosUserService.getAnunciosByUser().subscribe((response) => {
+    this.loadPage(this.currentPage);
+  }
+
+  loadPage(page: number): void {
+    this.currentPage = page;
+    this.anunciosUserService.getAnunciosByUser(page, this.pageSize).subscribe((response) => {
       this.anuncios = response.results;
+      this.totalCount = response.count;
     });
   }
 
   onAnuncioDeleted(anuncioId: number) {
     this.anuncios = this.anuncios.filter(anuncio => anuncio.id !== anuncioId);
+    this.totalCount = Math.max(0, this.totalCount - 1);
+  }
+
+  onPageSizeChange(size: number): void {
+    this.pageSize = size;
+    this.loadPage(1);
   }
 }
